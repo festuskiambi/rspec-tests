e9246ea9f60824064f9899ee81675d9d0d883f88
Festus kiambi <kiamfestoh@gmail.com>
2017-09-16 09:16:40 +0300

    test valid with first name,lastname and email

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe Contact do
-	it "is valid with a firstname,lastname and email"
+	it "is valid with a firstname,lastname and email" do
+		contact = Contact.new(
+		  firstname: 'John',
+		  lastname: 'Doe',
+		  email: 'test@example.com')
+        expect(contact).to be_valid
+	end	
 	it "is invalid without a firstname"
 	it "is invalid without a lastname"	
 	it "is invalid without an email address"
