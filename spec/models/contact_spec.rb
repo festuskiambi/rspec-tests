98b4ec9f240397c4defbf8ad959472c96e25d9d6
Festus kiambi <kiamfestoh@gmail.com>
2017-09-16 09:50:45 +0300

    test invalid without lastname

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -10,12 +10,16 @@ describe Contact do
 	end	
 
 	it "is invalid without a firstname" do
-		contact = Contact.new(
-		  firstname: nil	)
+		contact = Contact.new(firstname: nil	)
 		contact.valid?
 		expect(contact.errors[:firstname]).to include("can't be blank")
 	end	
-	it "is invalid without a lastname"	
+
+	it "is invalid without a lastname" do
+		contact = Contact.new(lastname: nil)
+		contact.valid?
+		expect(contact.errors[:lastname]).to include("can't be blank")
+	end	
 	it "is invalid without an email address"
 	it "is invalid with a duplicate email address"
 	it "returns a contact's full name as a string"
