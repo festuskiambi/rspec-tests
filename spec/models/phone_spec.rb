44847600c678585a1dd3aa326b5ded9557dd4751
Festus kiambi <kiamfestoh@gmail.com>
2017-09-19 15:06:59 +0300

    test allow two contacts to share a phone number

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -20,4 +20,26 @@ it "does not allow duplicate phone numbers" do
   expect(mobile_no.errors[:phone]).to include('has already been taken')  		
 end
 
+it "allows two contacts to share a phone number" do 
+	contact = Contact.create( 
+		firstname: 'John',
+		lastname: 'Doe',
+		email: 'test@example.com')
+    contact.phones.create(
+    	phone_type: 'home',
+    	phone: '0700 111 222')
+
+    contact_1 = Contact.new(
+    	firstname: 'Doe',
+    	lastname: 'John',
+    	email: 'test@example.com')
+
+    no = contact_1.phones.build(
+    	phone_type: 'home',
+    	phone: '0700 111 222')
+
+    
+    expect(no).to be_valid
+end    
+
 end
